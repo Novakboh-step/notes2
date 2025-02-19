9236a7a9f1da9c57dded3ebf72e5e1193a8fd82d
Novakboh-step <novak_dv82@student.itstep.org>
2025-02-19 11:24:43 +0200

    Added getNote function

--- NoteManager.py
+++ NoteManager.py
@@ -8,3 +8,16 @@ class NoteManager:
     def addNote(self, head, text):
         self.__list.append(Note(self.__freeID, head, text))
         self.__freeID += 1
+
+    def __findNote(self, id):
+        if id > len(self.__list) or id < 1:
+            return None
+        for i in range(id - 1, -1, -1):
+            if self.__list[i].getID() == id:
+                return i
+        return None
+
+    def getNote(self, id):
+        i = self.__findNote(id)
+        if i != None:
+            return self.__list[i].ToString()
